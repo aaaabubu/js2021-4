946e7fb48a1495b3a45bf7b8f6734bbbcabf80cf
ohgato <dhtmdduq99@naver.com>
2021-03-30 14:04:48 +0900

    3월 30일 요약정리

---
+++ code3-18.js
@@ -0,0 +1,12 @@
+const repl = require('relp');
+
+repl.start({
+  prompt: "숫자를 입력하세요 >",
+  eval: (command, context, filename, callback) => {
+    let (isNaN(number)) {
+
+    } else {
+      console.log("숫자입니다.");
+    }
+  } callback();
+}) 
